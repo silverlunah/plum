520bb7b94f3b22a6604fb121cf0729b2e22b5c1d
Jann <jann7x@gmail.com>
2025-05-03 20:12:40 +0900

    added license config and removed tests folder

--- backend/tests/features/LoginPage.feature
+++
@@ -1,6 +0,0 @@
-@suite-1
-Feature: Demo Sauce Login
-
-  @test-1
-  Scenario: Verify User Can Login Using Correct Details
-    Given I am in Demo Sauce Login page
--- backend/tests/step_definitions/LoginSteps.js
+++
@@ -1,9 +0,0 @@
-const { Given, When, Then } = require('@cucumber/cucumber');
-
-Given('I am in Demo Sauce Login page', async function () {
-	await this.loginPage.goToLoginPage();
-});
-
-Given('I fail this test', async function () {
-	throw new Error('err');
-});
